328521e6efeb5bab7f30d74486f9e4da595a7dfd
EC2 Default User <ec2-user@ip-172-31-85-8.ec2.internal>
2022-08-24 22:35:21 +0000

    completing project 4

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="superdalton/udacity4"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity4 --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity4 --address 0.0.0.0 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,12 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath="superdalton/udacity4"
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker tag udacity4 $dockerpath
+docker login 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
